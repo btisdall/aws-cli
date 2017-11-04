b11eb1358933d5266f45359274967398b73a2042
bentis <ben@tisdall.org.uk>
2017-11-04 15:48:05 +0100

    Add --no-reuse-parameters option.

--- awscli/customizations/cloudformation/deploy.py
+++ awscli/customizations/cloudformation/deploy.py
@@ -150,6 +150,16 @@ class DeployCommand(BasicCommand):
                 'Amazon Simple Notification Service topic Amazon Resource Names'
                 ' (ARNs) that AWS CloudFormation associates with the stack.'
             )
+        },
+        {
+            'name': 'no-reuse-parameters',
+            'action': 'store_true',
+            'required': False,
+            'default': False,
+            'help_text': (
+                'Do not automatically re-use a previous parameter value'
+                ' where a value has not been provided.'
+            )
         }
     ]
 
@@ -175,7 +185,10 @@ class DeployCommand(BasicCommand):
 
         template_dict = yaml_parse(template_str)
 
-        parameters = self.merge_parameters(template_dict, parameter_overrides)
+        parameters = self.merge_parameters(
+                    template_dict,
+                    parameter_overrides,
+                    parsed_args.no_reuse_parameters)
 
         deployer = Deployer(cloudformation_client)
         return self.deploy(deployer, stack_name, template_str,
@@ -206,13 +219,14 @@ class DeployCommand(BasicCommand):
         sys.stdout.flush()
         return 0
 
-    def merge_parameters(self, template_dict, parameter_overrides):
+    def merge_parameters(self, template_dict, parameter_overrides, no_reuse_parameters=False):
         """
         CloudFormation CreateChangeset requires a value for every parameter
         from the template, either specifying a new value or use previous value.
         For convenience, this method will accept new parameter values and
         generates a dict of all parameters in a format that ChangeSet API
-        will accept
+        will accept, unless no_reuse_parameters is true in which case all template
+        parameters lacking a default value must be provided at runtime.
 
         :param parameter_overrides:
         :return:
@@ -231,7 +245,10 @@ class DeployCommand(BasicCommand):
             if key in parameter_overrides:
                 obj["ParameterValue"] = parameter_overrides[key]
             else:
-                obj["UsePreviousValue"] = True
+                if no_reuse_parameters:
+                    continue
+                else:
+                    obj["UsePreviousValue"] = True
 
             parameter_values.append(obj)
 
